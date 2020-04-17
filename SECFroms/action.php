36f4970c3098e7cd5fd527c4da9623580d2e44a0
Hensikt <bjornemmaneel@gmail.com>
2020-04-17 15:43:22 +0200

    update

--- SECFroms/action.php
+++ SECFroms/action.php
@@ -1,4 +1,14 @@
 <?php
+header('poging xss protectie');
+$name = htmlspecialchars($_GET['field1'] . " " . $_GET['field2'], ENT_QUOTES, 'UTF-8');
+$email = htmlspecialchars($_GET['field3'], ENT_QUOTES, 'UTF-8');
+$subject = htmlspecialchars($_GET['field4'], ENT_QUOTES, 'UTF-8');;
+$message = htmlspecialchars($_GET['field5'], ENT_SUBSTITUTE, 'UTF-8');;
+
+echo "Hallo " . $name . '<br>' .
+     "Dit is uw email: " . $email . '<br>' .
+     "Dit heeft u geselecteerd: " . $subject . '<br>' .
+     "En dit is uw bericht -> " . $message;
 ?>
 <!DOCTYPE html>
 <html lang="en">
